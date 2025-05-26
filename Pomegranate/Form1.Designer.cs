5b2368de3e582b75f0068a89c980d05302f1315f
Leeky[Suya] <kocetovaanastasia19@gmail.com>
2025-05-26 13:33:45 +0300

    db

--- Pomegranate/Form1.Designer.cs
+++ Pomegranate/Form1.Designer.cs
@@ -29,24 +29,11 @@
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
-            AddData = new Button();
             dataGridView1 = new DataGridView();
             databaseManagerBindingSource = new BindingSource(components);
-            txtName = new TextBox();
-            label2 = new Label();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)databaseManagerBindingSource).BeginInit();
             SuspendLayout();
-            // 
-            // AddData
-            // 
-            AddData.BackColor = SystemColors.ActiveCaption;
-            AddData.Location = new Point(460, 376);
-            AddData.Name = "AddData";
-            AddData.Size = new Size(290, 62);
-            AddData.TabIndex = 0;
-            AddData.Text = "Добавить";
-            AddData.UseVisualStyleBackColor = false;
             // 
             // dataGridView1
             // 
@@ -56,7 +43,7 @@
             dataGridView1.Location = new Point(12, 12);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.Size = new Size(388, 426);
+            dataGridView1.Size = new Size(744, 620);
             dataGridView1.TabIndex = 1;
             dataGridView1.CellContentClick += dataGridView1_CellContentClick;
             // 
@@ -64,46 +51,22 @@
             // 
             databaseManagerBindingSource.DataSource = typeof(DatabaseManager);
             // 
-            // txtName
-            // 
-            txtName.BackColor = SystemColors.HotTrack;
-            txtName.Location = new Point(460, 213);
-            txtName.Name = "txtName";
-            txtName.Size = new Size(125, 27);
-            txtName.TabIndex = 3;
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(460, 164);
-            label2.Name = "label2";
-            label2.Size = new Size(49, 20);
-            label2.TabIndex = 5;
-            label2.Text = "Name";
-            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(label2);
-            Controls.Add(txtName);
+            BackColor = SystemColors.ButtonFace;
+            ClientSize = new Size(1107, 644);
             Controls.Add(dataGridView1);
-            Controls.Add(AddData);
             Name = "Form1";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ((System.ComponentModel.ISupportInitialize)databaseManagerBindingSource).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
-
-        private Button AddData;
         private DataGridView dataGridView1;
-        private TextBox txtName;
-        private Label label2;
         private BindingSource databaseManagerBindingSource;
     }
 }
